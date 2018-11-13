73871ab67a8c97912b876f72411a87b968f81715
NUBAH <v.nobu227@gmail.com>
2018-11-13 06:09:23 +0000

    Add user setting1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'purchase_items/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, except: [:destroy, :new]
 
   resources :purchases, only: [:index, :show] do
   	resources :purchase_items, only: [:index]
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   root 'pro_genres#index'
   get  '/products/index' => 'products#index' #管理者用のindex
   get '/pro_genres/research' => 'pro_genres#research'
+  get '/user/passchange' => 'user#passchange'
 end
