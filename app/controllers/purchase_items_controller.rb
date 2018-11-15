ef70590fa6b963b4767c6af7403b2df30851dfd3
m-tokumoto <beatbackbybeat@gmail.com>
2018-11-15 08:52:28 +0000

    11-15 徳本

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -1,4 +1,8 @@
 class PurchaseItemsController < ApplicationController
   def index
+  	@purchase = Purchase.find(params[:purchase_id])
+  	@purchase_items = Purchase.find(params[:purchase_id]).purchase_items.all
+  	@cart_items = Cart_item.find(@purchase_items.cart_item_id).all
+  	@products = Product.find(@cart_item.product_id).all
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@purchases = User.find(params[:id]).purchases.all
 	end
 
 	def create
