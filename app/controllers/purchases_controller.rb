6a876c2b4eaa1b8b53d4be009614739616680916
m-tokumoto <beatbackbybeat@gmail.com>
2018-11-18 08:54:35 +0000

    11-18 tokumoto

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,10 +2,15 @@ class PurchasesController < ApplicationController
 	def new
 		@cart = current_user.carts.last
 		@purchase = Purchase.new
+		@cart = current_user.carts.last
+		@cart_items = CartItem.where(cart_id: @cart.id)
 	end
 
 	def create
 		@purchase = Purchase.new(purchase_params)
+		@cart = current_user.carts.last
+		@cart_items = CartItem.where(cart_id: @cart.id)
+
 		@purchase.user_id = current_user.id
 		if @purchase.save
 			redirect_to arigatou_path
@@ -37,7 +42,13 @@ class PurchasesController < ApplicationController
 
 	private
 	def purchase_params
-		params.require(:purchase).permit(:user_id, :get_points, :pay_points, :cart_id, :pur_total, :pay, :status,:date, :pur_name, :pur_name_kana, :pur_email, :pur_address, :pur_post_code, :pur_tell)
+		params.require(:purchase).permit(:user_id, :get_points, :pay_points, :cart_id, :pur_total, :pay, :status,:date, :pur_name, 
+			:pur_name_kana, :pur_email, :pur_address, :pur_post_code, :pur_tell,
+			purchase_items_attributes: [:purchase_id, :cart_item_id, :sub_total, :amount])
+	end
+
+	def purchase_item_params
+		params.require(:purchase_item).permit(:purchase_id, :cart_item_id, :sub_total, :amount)
 	end
 
 end
