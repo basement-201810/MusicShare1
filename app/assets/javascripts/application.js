e58cbe1095607781e90e81aa8e69ded7fd0bb25e
NUBAH <v.nobu227@gmail.com>
2018-11-19 09:13:24 +0000

    try something hard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,13 @@
 //= require_tree .
 //= require nested_form_fields
 //= require jquery_ujs
+
+<script type="text/javascript">
+    var mytextbox = document.getElementById('mytext');
+    var mydropdown = document.getElementById('dropdown');
+
+    mydropdown.onchange = function(){
+          mytextbox.value = mytextbox.value  + this.value; //to appened
+         //mytextbox.innerHTML = this.value;
+    }
+</script>
