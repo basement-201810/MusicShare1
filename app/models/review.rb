c76d3de68019d6aae35d5b910253161a4974c46c
NUBAH <v.nobu227@gmail.com>
2018-11-22 09:37:56 +0000

    11-22

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
 	belongs_to :product
 	belongs_to :user
 	enum review_status: {release: true, nonrelease: false}
-	validates :review_body, presence: true
+	validates :review_body, length: { minimum: 0 }
+	belongs_to :purchase_item
 
 end
