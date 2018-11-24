fbc3d296847aef1e5888ce004db3f26621565616
AiMatsu <ngn.tk.0635@gmail.com>
2018-11-24 08:59:12 +0000

    validation/errer message/admin top

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
   has_many :carts
   enum user_status: {release: true, nonrelease: false}
 
-  # validates :name, presence: :true, length: {minimum: 2 ,maximum: 20 }
-  # validates :name_kana, presence: :true
-  # validates :email, presence: :true,uniqueness: true
-  # validates :address, presence: :true
-  # validates :post_code, presence: :true, numericality: { only_integer: true },length: { maximum: 10 }
-  # validates :tell, presence: :true, numericality: { only_integer: true },uniqueness: true #電話番号は同じものは登録できません
-  # paginates_per 5
+  validates :name,presence: :true,length: {minimum: 2 ,maximum: 20 }
+  validates :name_kana,presence: :true,format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "全角カタカナのみで入力して下さい"}
+  validates :email,presence: :true,uniqueness: true
+  validates :address,presence: :true
+  validates :post_code,presence: :true,numericality: { only_integer: true },length: { maximum: 10 }
+  validates :tell,presence: :true,numericality: { only_integer: true },uniqueness: true #電話番号は同じものは登録できません
+  paginates_per 5
   acts_as_paranoid
 end
