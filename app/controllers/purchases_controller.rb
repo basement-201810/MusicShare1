85fd8eff29b879ae8542dfb74f43dc344224dc4e
Wellstone515 <info.fullmoons@gmail.com>
2018-11-25 09:53:59 +0000

    11-25 conflict

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -116,3 +116,4 @@ class PurchasesController < ApplicationController
 	end
 
 end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2018_11_22_073705) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.integer "purchase_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
