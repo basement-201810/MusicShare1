06013498c1a62d8734026c83946bbe9ef9843352
Wellstone515 <info.fullmoons@gmail.com>
2018-11-25 02:24:25 +0000

    11-24 ishii

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,24 +14,23 @@ class UsersController < ApplicationController
 		@review = Review.new
 	end
 
+	def edit
+		@user = User.with_deleted.find(params[:id])
+	end
+
 	def update
-		@user = User.find(params[:id])
-		if @user.update!(user_params)
-			redirect_to user_path, notice: 'Profile was successfully updated!!'
+		@user = User.with_deleted.find(params[:id])
+		if @user.update(user_params)
+			redirect_to user_path, notice: 'ユーザー情報を編集しました！'
 		else
 			render :edit
 		end
 	end
 
-
-	def edit
-		@user = User.with_deleted.find(params[:id])
-	end
-
 	def destroy
 		user = User.find(params[:id])
 		user.destroy
-		redirect_to root_path
+		redirect_to users_path
 	end
 
 	# def release
