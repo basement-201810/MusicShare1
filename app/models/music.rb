1b37160be6afba6a0e532cbc9a97d66faefb81d6
NUBAH <v.nobu227@gmail.com>
2018-11-27 07:28:17 +0000

    wtf

--- app/models/music.rb
+++ app/models/music.rb
@@ -3,7 +3,6 @@ class Music < ApplicationRecord
 
     validates :music_name, presence: true
     validates :music_disk_number, presence: true
-    validates :product_id, presence: true
     validates :music_number, presence: true
 
 end
